94c1949ee71c692900f20668813c4c1a9d709aba
Yohanan <harrochy96@gmail.com>
2019-01-30 20:22:13 +0100

    implementation of the wrapper for DES

---
+++ src/wrapper_des.rs
@@ -0,0 +1,11 @@
+extern {
+    fn DES(plain: &[u64; 64], key: &[u64; 56], cipher: &[u64; 64]);
+}
+
+pub fn des(plain: [u64; 64], key: [u64; 56], cipher: [u64; 64]) {
+
+    unsafe {
+        DES(&plain, &key, &cipher);
+    }
+    println!("hello from wrapper_des");
+}
